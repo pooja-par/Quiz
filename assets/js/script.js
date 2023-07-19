30e1bcaf89818835dba89479e791ea561caaea88
pooja-par <>
2023-07-19 04:06:15 +0000

    this is example of commit message

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,4 +8,38 @@ const questions = [
             { text: "Bangladesh", correct: false },
         ]
 
-    },   
+    }, 
+
+    {
+        question: "Ben Stokes made his Test debut for England in which year?",// Second question
+        answers: [
+            { text: 2002, correct: false },
+            { text: 2012, correct: false },
+            { text: 2013, correct: true },
+            { text: 2005, correct: false },
+        ]
+
+    }, 
+
+    {
+        question: "Who was the first player to reach 10,000 runs in Test cricket?",// Third question
+        answers: [
+            { text: "Sunil gavasker", correct: false },
+            { text: "M. S. Dhoni", correct: false },
+            { text: "AB de Villiers", correct: true },
+            { text: "Shane Warne", correct: false },
+        ]
+
+    },
+
+    {
+        question: "How many runs did Jack Leach score during his iconic second innings with Ben Stokes during the 2019 Ashes Third Test at Headingley?",// Fourth question
+        answers: [
+            { text: 6, correct: false },
+            { text: 0, correct: false },
+            { text: 3, correct: true },
+            { text: 1, correct: false },
+        ]
+
+    },
+
