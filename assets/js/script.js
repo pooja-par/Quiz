ae6f476dfa499bf129abb9e7d41aca21e32be9f4
pooja-par <>
2023-07-19 04:03:55 +0000

    this is example of commit message

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+const questions = [
+    {
+        question: "The Big Bash League is based in which country?",// First question
+        answers: [
+            { text: "India", correct: false },
+            { text: "Australia", correct: true },
+            { text: "Sri Lanka", correct: false },
+            { text: "Bangladesh", correct: false },
+        ]
+
+    },   
